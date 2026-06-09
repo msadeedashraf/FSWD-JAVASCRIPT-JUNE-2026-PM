1467ebf8e50de2b9074ad0e0a22514bbfc3780e6
Sadeed <msadeedashraf@gmail.com>
2026-06-09 19:32:02 -0400

    .

---
+++ Day-02/script.js
@@ -0,0 +1,21 @@
+//alert("Im testing my JS Page")
+
+/*
+function nameofyourfunction()
+{
+
+}
+*/
+
+
+function myGreetings()
+{
+
+    document.getElementById('mymessage').innerHTML = 'Welcome to learning React'
+}
+
+
+function changeDate()
+{
+    document.getElementById("mydate").innerHTML = Date()
+}
